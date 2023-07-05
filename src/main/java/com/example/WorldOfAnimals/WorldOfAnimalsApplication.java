f1ebdaecc83ad08f9d433ee3dc024744e786025e
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-07-05 12:55:59 +0300

    Init commit

---
+++ src/main/java/com/example/WorldOfAnimals/WorldOfAnimalsApplication.java
@@ -0,0 +1,13 @@
+package com.example.WorldOfAnimals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldOfAnimalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldOfAnimalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WorldOfAnimals/WorldOfAnimalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WorldOfAnimals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldOfAnimalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
