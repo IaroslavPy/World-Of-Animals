53ef3ece1441c8222541d3b46057ff286509f4d8
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-07-05 13:06:27 +0300

    Add new entity into models

---
+++ src/main/java/com/example/WorldOfAnimals/models/AnimalBehavior.java
@@ -0,0 +1,16 @@
+package com.example.WorldOfAnimals.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnimalBehavior {
+
+    private String type;
+    private String description;
+}
