5bd995b47020dfc438b32ba286cb03cc0af806a7
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-07-05 13:11:35 +0300

    Add controller

---
+++ src/main/java/com/example/WorldOfAnimals/controllers/AnimalsController.java
@@ -0,0 +1,32 @@
+package com.example.WorldOfAnimals.controllers;
+
+
+import com.example.WorldOfAnimals.models.AnimalBehavior;
+import com.example.WorldOfAnimals.services.AnimalsService;
+import com.example.WorldOfAnimals.utils.RandomPositionEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AnimalsController {
+
+    @Autowired
+    AnimalsService service;
+    @Autowired
+    RandomPositionEnum randomAnimal;
+
+    @GetMapping
+    public AnimalBehavior getAnimal() {
+        return service.getAnimalBehavior(randomAnimal.getAnimal());
+    }
+
+    @GetMapping("/animals")
+    public String getAnimals() {
+        return service.getAnimals();
+    }
+
+
+}
