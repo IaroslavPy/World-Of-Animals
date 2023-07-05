518e64fb15310eacb676d98ddd2bd824f21b9fdc
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-07-05 13:40:08 +0300

    Add entity AnimalsGroup

---
+++ src/main/java/com/example/WorldOfAnimals/models/AnimalsGroup.java
@@ -0,0 +1,17 @@
+package com.example.WorldOfAnimals.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+
+// Created for view as JSON
+public class AnimalsGroup {
+
+    private String animalsType;
+}
