eecf99ca848652ae93b90a87092019b8abdcc395
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-07-06 11:22:53 +0300

    Delete randomUtil

--- src/main/java/com/example/WorldOfAnimals/controllers/AnimalsController.java
+++ src/main/java/com/example/WorldOfAnimals/controllers/AnimalsController.java
@@ -4,7 +4,6 @@ package com.example.WorldOfAnimals.controllers;
 import com.example.WorldOfAnimals.models.AnimalBehavior;
 import com.example.WorldOfAnimals.models.AnimalsGroup;
 import com.example.WorldOfAnimals.services.AnimalsService;
-import com.example.WorldOfAnimals.utils.RandomPositionEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +15,11 @@ public class AnimalsController {
 
     @Autowired
     private AnimalsService service;
-    @Autowired
-    private RandomPositionEnum randomAnimal;
+
 
     @GetMapping
     public AnimalBehavior getAnimal() {
-        return service.getAnimalBehavior(randomAnimal.getAnimal());
+        return service.getAnimalBehavior();
     }
 
     @GetMapping("/animals")
