b9fc5d98ea25a415d4e3862b9ee3ae7368ce9ddc
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-07-07 10:36:13 +0300

    Change method getAnimal

--- src/main/java/com/example/WorldOfAnimals/controllers/AnimalsController.java
+++ src/main/java/com/example/WorldOfAnimals/controllers/AnimalsController.java
@@ -5,6 +5,7 @@ import com.example.WorldOfAnimals.models.AnimalBehavior;
 import com.example.WorldOfAnimals.models.AnimalsGroup;
 import com.example.WorldOfAnimals.services.AnimalsService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,7 +24,12 @@ public class AnimalsController {
     }
 
     @GetMapping("/{name}")
-    public AnimalBehavior getAnimal(@PathVariable("name") String nameAnimal) {
-        return service.getAnimalBehavior(nameAnimal);
+    public ResponseEntity<AnimalBehavior> getAnimal(@PathVariable("name") String nameAnimal) {
+        try {
+            AnimalBehavior animalBehavior = service.getAnimalBehavior(nameAnimal);
+            return ResponseEntity.ok(animalBehavior);
+        } catch (AnimalNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
