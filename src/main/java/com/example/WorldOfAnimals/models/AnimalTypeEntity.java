5046044058c8d3775fbe926004902c183764d333
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-07-07 20:47:07 +0300

    Add entity AnimalTypeEntity

---
+++ src/main/java/com/example/WorldOfAnimals/models/AnimalTypeEntity.java
@@ -0,0 +1,14 @@
+package com.example.WorldOfAnimals.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class AnimalTypeEntity {
+
+    private int id;
+    private String name;
+}
