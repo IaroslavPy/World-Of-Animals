4b29e711fe4f9850261284566beab4262191d8d2
Iaroslav <121291798+IaroslavPy@users.noreply.github.com>
2023-07-07 12:25:32 +0300

    Feature (#1)
    * Add .yml and port
    
    * Add models (Enum)
    
    * Add new entity into models
    
    * Add util
    
    * Add service
    
    * Add controller
    
    * Add entity AnimalsGroup
    
    * Change method getAnimals (services, controllers)
    
    * Add readme.txt (requests)
    
    * Change modifier, delete annotations
    
    * Change form .yml
    
    * Delete randomUtil
    
    * Change method getAnimal
    
    * Update and replace README.md
    
    * Add constructor for DI
    
    * Add path into .yml
    
    * Rename argument's name
    
    * Change endpoint name
    
    * Replace constructor on annotation
    
    * Add static method getAnimal
    
    * Change method getAnimalBehavior
    
    * Change method getAnimal
    
    * Change method getAnimals, add exception
    
    * Change method getAnimals (controller)
    
    * Add field for constructor
    
    * Add constructor into exception
    
    * Change parent exception

---
+++ src/main/java/com/example/WorldOfAnimals/models/Animals.java
@@ -0,0 +1,32 @@
+package com.example.WorldOfAnimals.models;
+
+import com.example.WorldOfAnimals.exceptions.AnimalNotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Arrays;
+
+@Getter
+@AllArgsConstructor
+public enum Animals {
+
+    WOLF("I can bite!"),
+    CAT("I can sleep!"),
+    DOG("I can walk!"),
+    RABBIT("I can eat!"),
+    HORSE("I can ride"),
+    COW("I make milk!"),
+    FOX("I can run!"),
+    SNAKE("I can see!"),
+    LEOPARD("I can hunt"),
+    SHEEP("I can sound");
+
+    private final String behavior;
+
+    public static Animals getAnimal(String type) throws AnimalNotFoundException {
+        return Arrays.stream(Animals.values())
+                .filter(animal -> animal.name().equals(type.toUpperCase()))
+                .findFirst()
+                .orElseThrow(() -> new AnimalNotFoundException("Not fund type=" + type));
+    }
+}
