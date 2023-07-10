87e94e065bc08df4509f9890c398d3010ebf5dab
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-07-10 10:50:54 +0300

    Add JPA annotations

--- src/main/java/com/example/WorldOfAnimals/models/AnimalBreed.java
+++ src/main/java/com/example/WorldOfAnimals/models/AnimalBreed.java
@@ -1,15 +1,29 @@
 package com.example.WorldOfAnimals.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
 public class AnimalBreed {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column
     private String name;
+
+    @Column
+    @ManyToOne
+    @JoinColumn
+    // These annotations without parameter because I will know them after check
     private AnimalTypeEntity type;
 }
