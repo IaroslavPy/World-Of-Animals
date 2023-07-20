30dc624b6d1a669fb8a772f00ee0cf0a725bee74
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-07-20 17:15:56 +0300

    Update HttpStatus into methods controller

--- src/main/java/com/example/WorldOfAnimals/controllers/AnimalTypeController.java
+++ src/main/java/com/example/WorldOfAnimals/controllers/AnimalTypeController.java
@@ -4,6 +4,7 @@ import com.example.WorldOfAnimals.exceptions.AnimalTypeNotFoundException;
 import com.example.WorldOfAnimals.models.AnimalTypeEntity;
 import com.example.WorldOfAnimals.services.AnimalTypeService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,8 +26,9 @@ public class AnimalTypeController {
     private final AnimalTypeService service;
 
     @PostMapping
-    public AnimalTypeEntity saveAnimalType(@RequestBody AnimalTypeEntity animalType) {
-        return service.saveOrUpdate(animalType);
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveAnimalType(@RequestBody AnimalTypeEntity animalType) {
+        service.saveOrUpdate(animalType);
     }
 
     @GetMapping
@@ -44,11 +47,13 @@ public class AnimalTypeController {
     }
 
     @PutMapping
-    public AnimalTypeEntity updateAnimalType(@RequestBody AnimalTypeEntity animalType) {
-        return service.saveOrUpdate(animalType);
+    @ResponseStatus(HttpStatus.OK)
+    public void updateAnimalType(@RequestBody AnimalTypeEntity animalType) {
+        service.saveOrUpdate(animalType);
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public void deleteAnimalTypeById(@PathVariable(value = "id") Integer id) {
         service.deleteAnimalTypeById(id);
     }
