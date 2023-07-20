1a15760b55d3ba09bf0ccf9da2186e69b5951435
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-07-20 09:54:07 +0300

    Replacement CrudRepository to JpaRepository

--- src/main/java/com/example/WorldOfAnimals/repositories/AnimalTypeRepository.java
+++ src/main/java/com/example/WorldOfAnimals/repositories/AnimalTypeRepository.java
@@ -1,14 +1,14 @@
 package com.example.WorldOfAnimals.repositories;
 
 import com.example.WorldOfAnimals.models.AnimalTypeEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface AnimalTypeRepository extends CrudRepository<AnimalTypeEntity, Integer> {
+public interface AnimalTypeRepository extends JpaRepository<AnimalTypeEntity, Integer> {
 
     AnimalTypeEntity save(AnimalTypeEntity animalType);
 
