65fff98fb21cbc60beaaffa0d6b7a9a468e54979
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-08-14 16:41:04 +0300

    Add Entity Animal

---
+++ src/main/java/com/example/WorldOfAnimals/models/Animal.java
@@ -0,0 +1,50 @@
+package com.example.WorldOfAnimals.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "animals")
+public class Animal {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne(targetEntity = AnimalTypeEntity.class, optional = false)
+    @JoinColumn(name = "type_id")
+    private AnimalTypeEntity animalTypeEntity;
+
+    @ManyToOne(targetEntity = AnimalBreedEntity.class, optional = false)
+    @JoinColumn(name = "breed_id")
+    private AnimalBreedEntity animalBreedEntity;
+
+    @Column(name = "created", nullable = false)
+    private Timestamp created;
+
+    @Column(name = "updated", nullable = false)
+    private Timestamp updated;
+}
+
