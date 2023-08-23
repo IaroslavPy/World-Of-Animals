1bf42ef5b0a3c38061e4aa68af24887976037138
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-08-23 12:24:53 +0300

    Create AnimalImageEntity

---
+++ src/main/java/com/example/WorldOfAnimals/models/AnimalImageEntity.java
@@ -0,0 +1,49 @@
+package com.example.WorldOfAnimals.models;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "animal_images")
+public class AnimalImageEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "animal_id")
+    private AnimalEntity animal;
+
+    @Column(name = "file_path")
+    private String filePath;
+
+    @Column(name = "type")
+    private String fileType;
+
+    @Column(name = "size")
+    private Long fileSize;
+
+    @Column(name = "created", nullable = false)
+    private Timestamp created;
+
+    @Column(name = "updated", nullable = false)
+    private Timestamp updated;
+}
