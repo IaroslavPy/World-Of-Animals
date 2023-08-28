74842e31ddb51964aba3338bd5ae79b1ee2bbc56
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-08-28 10:27:34 +0300

    Create AnimalImageService

---
+++ src/main/java/com/example/WorldOfAnimals/services/AnimalImageService.java
@@ -0,0 +1,14 @@
+package com.example.WorldOfAnimals.services;
+
+import com.example.WorldOfAnimals.models.AnimalImageEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class AnimalImageService {
+
+    public AnimalImageEntity uploadImage(MultipartFile file){
+        return null;
+    }
+
+}
