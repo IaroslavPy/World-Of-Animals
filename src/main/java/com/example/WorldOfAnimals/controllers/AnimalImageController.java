28d5a650ab16b395c0b5547ef9ac6b6cc52c01a6
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-08-28 10:32:35 +0300

    Create AnimalImageController

---
+++ src/main/java/com/example/WorldOfAnimals/controllers/AnimalImageController.java
@@ -0,0 +1,11 @@
+package com.example.WorldOfAnimals.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/animals/images")
+@AllArgsConstructor
+public class AnimalImageController {
+}
