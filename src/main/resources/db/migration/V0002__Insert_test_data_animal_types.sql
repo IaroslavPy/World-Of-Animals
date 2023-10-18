44dad473a9af350c904e530c4d8c3026cad29d98
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-10-18 10:18:52 +0300

    Add Flyway migration (animal_types,animal_breeds)

---
+++ src/main/resources/db/migration/V0002__Insert_test_data_animal_types.sql
@@ -0,0 +1,12 @@
+INSERT INTO `animal_types`
+VALUES (2,'Cat'),
+(4,'Cow'),
+(1,'Dog'),
+(5,'Rabbit'),
+(13,'Snake'),
+(10,'Snake1'),
+(23,'SuperSnake1'),
+(15,'TestPuT'),
+(7,'Tiger'),
+(3,'Wolf'
+);
