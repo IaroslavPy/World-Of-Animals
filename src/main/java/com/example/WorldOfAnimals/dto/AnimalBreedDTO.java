e01a074a02e39b4ae11fc625968c5360fe0b4687
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-10-20 16:01:28 +0300

    Add description to AnimalBreedDTO

--- src/main/java/com/example/WorldOfAnimals/dto/AnimalBreedDTO.java
+++ src/main/java/com/example/WorldOfAnimals/dto/AnimalBreedDTO.java
@@ -1,19 +1,24 @@
 package com.example.WorldOfAnimals.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(description = "Animal Breed Model for Response")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class AnimalBreedDTO {
 
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY, description = "Breed ID")
     private Integer id;
 
+    @Schema(description = "Breed name", example = "eskimo")
     private String name;
 
+    @Schema(description = "Entity type of animal (ID and name)")
     private AnimalTypeDTO animalTypeDTO;
 }
