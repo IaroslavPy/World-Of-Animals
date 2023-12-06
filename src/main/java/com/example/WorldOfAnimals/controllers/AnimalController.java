0373c8e59cee46dcfa932930addd9dd74f39f475
IaroslavPy <iaroslavpylypchuk@gmail.com>
2023-12-06 12:48:08 +0200

    Modify Pagination method

--- src/main/java/com/example/WorldOfAnimals/controllers/AnimalController.java
+++ src/main/java/com/example/WorldOfAnimals/controllers/AnimalController.java
@@ -6,13 +6,13 @@ import com.example.WorldOfAnimals.dto.AnimalRequestPutDTO;
 import com.example.WorldOfAnimals.dto.ErrorResponseDTO;
 import com.example.WorldOfAnimals.services.AnimalService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -84,7 +83,8 @@ public class AnimalController {
     })
     @GetMapping
     public Page<AnimalDTO> getPaginatedAnimals(
-            @PageableDefault(size = 5, sort = "name") @NotNull Pageable pageable) {
+            @PageableDefault(size = 5, sort = "name")
+            @ParameterObject @NotNull Pageable pageable) {
         return service.getPaginatedAnimals(pageable);
     }
 
